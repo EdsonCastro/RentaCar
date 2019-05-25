090c8fc063b475d26a1452c5d7e5c1f00edd70f9
EdsonCastro <edson.sv@gmail.com>
2019-05-25 21:47:51 +0200

    Funtionality of Put on Client writing in BD.

--- src/main/java/com/example/rentacar/controller/ClientController.java
+++ src/main/java/com/example/rentacar/controller/ClientController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -48,5 +49,16 @@ public class ClientController {
 		clientDto.setNameClient(clientEntity.getNameClient());
 		return clientDto;
 	}
+	
+	@PutMapping("/{id}")
+	public ClientDto put(@RequestBody ClientDto clientDto){
+		ClientEntity clientEntity = clientRepository.getOne(clientDto.getIdClient());
+		clientEntity.setNameClient(clientDto.getNameClient());
+		clientRepository.saveAndFlush((ClientEntity) clientEntity);
+		clientDto.setIdClient(clientEntity.getIdclient());
+		clientDto.setNameClient(clientEntity.getNameClient());			
+		return clientDto;
+	}
+	
 	
 }
